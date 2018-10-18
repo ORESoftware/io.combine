866eec30c10906baa1844fb6574fefde2a994aa6
Alexander Mills <alex@oresoftware.com>
2018-10-17 17:30:42 -0700

    ores/auto-commit => 'set'

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,52 @@
 'use strict';
 
-export const foo = 'bar';
+import * as readline from 'readline';
+import {Readable, Transform, Writable} from "stream";
+
 
 export const r2gSmokeTest = function () {
   // r2g command line app uses this exported function
   return true;
 };
 
-/*
+const flattenDeep = (arr: Array<any>): Array<any> => {
+  return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+};
 
- bad library design:
- module.exports = 'foo';
- export = 'foo';
-
- good library design:
- exports.x = 'foo'
- export const x = 'foo'
- export default = 'foo';
-
-*/
-
+export class Combiner {
+  
+  private trans: Transform;
+  private readonly writable: Writable;
+  
+  constructor(w: Writable) {
+    
+    this.writable = w;
+    
+    this.trans = new Transform({
+      transform(chunk, enc, cb) {
+        cb(null, String(chunk));
+      }
+    });
+    
+    this.trans.cork();
+  }
+  
+  start(): this {
+    this.trans.pipe(this.writable, {end: false});
+    return this;
+  }
+  
+  combine(readable: Readable | Array<Readable>): this {
+    
+    for (let v of flattenDeep([readable])) {
+      readline.createInterface({input: v}).on('line', l => {
+        this.trans.write(l + '\n');
+      });
+    }
+    
+    return this;
+  }
+  
+}
 
 
